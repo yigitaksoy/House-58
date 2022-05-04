f8a7227333ac8d33104b4a094aff4490dea3ca17
yigitaksoy <yigitaksoy@gmail.com>
2022-05-04 14:04:00 +0200

    Add tech stack logos

---
+++ src/data/TechLogos.js
@@ -0,0 +1,38 @@
+import ReactLogo from "../assets/logos/ReactLogo.svg";
+import DjangoLogo from "../assets/logos/Django-logo.png";
+import AwsLogo from "../assets/logos/aws.svg";
+import WordpressLogo from "../assets/logos/WordPress.png";
+import ShopifyLogo from "../assets/logos/shopify.png";
+
+export const techLogos = [
+  {
+    id: 1,
+    img: ReactLogo,
+    alt: "React",
+    width: 100,
+  },
+  {
+    id: 3,
+    img: DjangoLogo,
+    alt: "Django",
+    width: 100,
+  },
+  {
+    id: 4,
+    img: AwsLogo,
+    alt: "Aws",
+    width: 100,
+  },
+  {
+    id: 5,
+    img: WordpressLogo,
+    alt: "Wordpress",
+    width: 150,
+  },
+  {
+    id: 6,
+    img: ShopifyLogo,
+    alt: "Shopify",
+    width: 100,
+  },
+];
