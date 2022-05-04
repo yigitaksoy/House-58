5a16eb9e590f74014baf898fd0d81edb34ffe648
yigitaksoy <yigitaksoy@gmail.com>
2022-05-04 14:12:23 +0200

    Add services list data

---
+++ src/data/ServicesList.js
@@ -0,0 +1,35 @@
+import Ecommerce from "../assets/svgs/ecommerce.svg";
+import Maintanence from "../assets/svgs/maintanence.svg";
+import Undraw from "../assets/svgs/web-design.svg";
+import FullStack from "../assets/svgs/full-stack.svg";
+
+export const servicesList = [
+  {
+    id: 1,
+    title: "Full Stack Web Development",
+    description:
+      "The online presence helps any business to open ample opportunities for its growth. Based on your individual needs, we can help you to build a modern web solution.",
+    img: FullStack,
+  },
+  {
+    id: 2,
+    title: "Ecommerce",
+    description:
+      "We build eCommerce websites that increase revenue and operational efficiency. Our clients sell across multiple channels and grow their businesses faster by easily managing their inventories and website all from one personalised platform.",
+    img: Ecommerce,
+  },
+  {
+    id: 3,
+    title: "Web Design",
+    description:
+      "Your website is one of the first things that your potential customers will see when they’re searching for a company like yours online. We provide a full range of custom web application development services starting from UI/UX design, to front-end and back-end development, maintenance and support.",
+    img: Undraw,
+  },
+  {
+    id: 4,
+    title: "Website Maintanence",
+    description:
+      "We make sure that your website runs reliably, securely, and blazing fast so that you can focus on what matters, which is growing your business. House 58 can also keep your website up to date with fresh content and features.",
+    img: Maintanence,
+  },
+];
