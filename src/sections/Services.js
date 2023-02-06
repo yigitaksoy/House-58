a7a81e6afc9dc6c63f1f3e815955134746ee313b
yigitaksoy <yigitaksoy@gmail.com>
2023-02-06 03:58:06 +0100

    Change services section layout

--- src/sections/Services.js
+++ src/sections/Services.js
@@ -16,87 +16,63 @@ const Services = () => {
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 0.3, delay: 0.5 }}
-          className=" font-black lg:text-7xl text-5xl text-center pt-20 text-cool-white mb-20"
+          className="font-black lg:text-4xl text-2xl text-center mt-20 text-cool-white mb-20 services-title"
         >
-          What we do
+          * Take a peek into our bag of tricks
         </motion.h2>
       </motion.div>
       <div className="mx-auto p-5">
-        {servicesList.map((services) =>
-          services.id % 2 != 0 ? (
+        {servicesList.map((services, index) => (
+          <motion.div
+            key={index}
+            className="flex flex-wrap pt-10 pb-10 items-center glow md:flex-row-reverse"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            viewport={{ once: true }}
+          >
             <motion.div
               key={services.id}
-              className="flex flex-wrap pt-10 pb-10 items-center glow md:flex-row-reverse mb-20 lg:shadow-2xl rounded-3xl"
+              className="mx-auto"
               initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{ duration: 0.3, delay: 0.7 }}
+              whileInView={{
+                opacity: 1,
+              }}
+              viewport={{ once: true }}
+              transition={{
+                ease: "easeInOut",
+                duration: 0.5,
+              }}
             >
-              <motion.div className="w-full md:w-1/2 lg:w-5/12 mr-auto md:mr-auto">
-                <img
-                  className="w-full mx-auto mb-12 md:float-right max-w-xs lg:max-w-sm"
-                  src={services.img}
-                  alt={services.title}
-                />
-              </motion.div>
-              <motion.div
-                key={services.id}
-                className="w-full md:w-1/2 lg:pl-1 mx-auto "
-                initial={{ opacity: 0 }}
-                whileInView={{
-                  opacity: 1,
-                }}
-                viewport={{ once: true }}
-                transition={{
-                  ease: "easeInOut",
-                  duration: 0.5,
-                }}
-              >
-                <motion.h3 className="font-black lg:text-5xl pb-8 text-5xl md:text-4xl md:text-center">
-                  {services.title}
-                </motion.h3>
-                <motion.p className="text-cool-white font-black text-xl lg:text-2xl  md:text-lg mx-auto mb-5">
-                  {services.description}
-                </motion.p>
-              </motion.div>
+              <motion.h3 className="font-black lg:text-8xl text-4xl text-center xl md:text-4xl md:text-center uppercase text-cool-white">
+                {services.title}
+              </motion.h3>
+              <div className="max-w-screen-xl mx-auto text-sm flex flex-col items-center">
+                <div className="py-5 mx-auto text-center lg:pl-40 lg:pr-40">
+                  <motion.p className="text-cool-white text-xl lg:text-2xl  md:text-lg mx-auto mb-5">
+                    {services.description}
+                  </motion.p>
+                  <motion.div className="mt-5 lg:mt-10">
+                    <motion.ul className="text-cool-white list-disc  text-xl lg:text-xl md:text-lg mb-5 flex flex-wrap lg:flex-row lg:-mx-5">
+                      {services.details &&
+                        services.details.map((detail, index) => (
+                          <motion.li
+                            className={`text-cool-white text-xl list-inside marker:text-cool-blue lg:text-2xl md:text-lg mb-5 w-full lg:w-1/2 lg:px-5 ${
+                              index % 2 === 0 ? "lg:pr-5" : "lg:pl-5"
+                            }`}
+                            key={index}
+                          >
+                            {detail.option}
+                          </motion.li>
+                        ))}
+                    </motion.ul>
+                  </motion.div>
+                </div>
+              </div>
+              <hr className="border-1 mx-auto w-1/2 services-border" />
             </motion.div>
-          ) : (
-            <motion.div
-              key={services.id}
-              className="flex flex-wrap pt-10 pb-10 items-center md:flex-row-reverse mb-20 "
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{ duration: 0.3, delay: 0.5 }}
-            >
-              <motion.div className="w-full md:w-1/2 lg:w-5/12 ml-auto md:ml-auto md:order-last">
-                <img
-                  className="w-full mx-auto mb-12 md:float-left max-w-xs lg:max-w-sm "
-                  src={services.img}
-                  alt={services.title}
-                />
-              </motion.div>
-              <motion.div
-                key={services.id}
-                className="w-full md:w-1/2 mx-auto"
-                initial={{ opacity: 0 }}
-                whileInView={{
-                  opacity: 1,
-                }}
-                viewport={{ once: true }}
-                transition={{
-                  ease: "easeInOut",
-                  duration: 0.5,
-                }}
-              >
-                <motion.h3 className="font-black lg:text-5xl pb-8 text-5xl md:text-4xl md:text-center">
-                  {services.title}
-                </motion.h3>
-                <motion.p className="text-cool-white font-black text-xl lg:text-2xl  md:text-lg mx-auto mb-5">
-                  {services.description}
-                </motion.p>
-              </motion.div>
-            </motion.div>
-          )
-        )}
+          </motion.div>
+        ))}
       </div>
     </motion.div>
   );
