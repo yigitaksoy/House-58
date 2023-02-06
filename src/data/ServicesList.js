1117c316f433f642fc02979b2b4341a015555179
yigitaksoy <yigitaksoy@gmail.com>
2023-02-06 03:52:41 +0100

    Update services details

--- src/data/ServicesList.js
+++ src/data/ServicesList.js
@@ -6,25 +6,42 @@ import FullStack from "../assets/svgs/full-stack.svg";
 export const servicesList = [
   {
     id: 1,
-    title: "Full Stack Web Development",
+    title: "Web Dev",
     description:
-      "The online presence helps any business to open ample opportunities for its growth. Based on your individual needs, we can help you to build a modern web solution.",
-    img: FullStack,
+      "Your website is one of the first things that your potential customers will see when they’re searching for a company like yours online. We provide a full range of custom web application development services starting from UI/UX design, to front-end and back-end development, maintenance and support.",
+    details: [
+      { id: 1, option: "Responsive web development" },
+      { id: 2, option: "Web applications" },
+      { id: 3, option: "UX consulting" },
+      { id: 3, option: "Maintenance & support" },
+    ],
+    img: WebDesign,
   },
   {
     id: 2,
-    title: "Ecommerce",
+    title: "E-commerce",
     description:
-      "We build eCommerce websites that increase revenue and operational efficiency. Our clients sell across multiple channels and grow their businesses faster by easily managing their inventories and website all from one personalised platform.",
+      "Shopping made easy for buyers and sellers. We’ll give your online shop the illusion of a physical storefront. Our clients sell across multiple channels and grow their businesses faster by easily managing their inventories and website all from one personalised platform.",
+    details: [
+      {
+        id: 1,
+        option: "WooCommerce",
+      },
+      {
+        id: 1,
+        option: "Shopify",
+      },
+    ],
     img: Ecommerce,
   },
   {
     id: 3,
-    title: "Web Design",
+    title: "Full Stack Development",
     description:
-      "Your website is one of the first things that your potential customers will see when they’re searching for a company like yours online. We provide a full range of custom web application development services starting from UI/UX design, to front-end and back-end development, maintenance and support.",
-    img: WebDesign,
+      "The online presence helps any business to open ample opportunities for its growth. Based on your individual needs, we can help you to build a modern web solution.",
+    img: FullStack,
   },
+
   {
     id: 4,
     title: "Website Maintanence",
