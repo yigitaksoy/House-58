4739fa375541ea1fe170059b4a5e61bfbf64d231
yigitaksoy <yigitaksoy@gmail.com>
2023-05-09 01:40:47 +0300

    Update hover effect on lets talk link

--- src/sections/ServicesIntro.js
+++ src/sections/ServicesIntro.js
@@ -1,16 +1,24 @@
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import { useRef } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 
 const ServicesIntro = () => {
   const intersectTarget = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
 
   const { ref, inView } = useInView({
     threshold: 0.1, // Can be set between 0 to 2
   });
   const animation = useAnimation();
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   useEffect(() => {
     if (inView) {
@@ -49,17 +57,20 @@ const ServicesIntro = () => {
             And create sustainable digital products with a{" "}
             <motion.span className="story">story!</motion.span>
           </motion.p>
-          <motion.p whileHover={{ scale: 1.2, x: 80 }}>
-            <Link
-              to="/contact"
-              className="text-cool-blue3 text-2xl lg:text-3xl"
-            >
+          <Link
+            to="/contact"
+            className="text-cool-blue3 text-2xl lg:text-3xl"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleMouseLeave}
+          >
+            <p>
               Lets Talk!
-              <svg
+              <motion.svg
+                animate={isHovered ? { x: 5 } : { x: 0 }}
                 className="w-6 h-6 inline ml-5"
                 fill="none"
                 stroke="currentColor"
-                viewBox="0 0 24 24"
+                viewBox="0 0 23 23"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
@@ -68,9 +79,9 @@ const ServicesIntro = () => {
                   strokeWidth={2}
                   d="M14 5l7 7m0 0l-7 7m7-7H3"
                 />
-              </svg>
-            </Link>
-          </motion.p>
+              </motion.svg>
+            </p>
+          </Link>
         </motion.div>
       </div>
     </motion.div>
